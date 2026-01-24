9cc04947ad3c72aaa9c4c087d5f33663a53ba09f
Samantha Morales <samanthamp43@gmail.com>
2026-01-23 22:52:53 -0600

    refactored code folder, progress on hw2

---
+++ code/hw2/plot.py
@@ -0,0 +1,67 @@
+from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib.colors import ListedColormap as lcm
+
+######################## QUESTION 1 #############################
+# x = np.linspace(-5 * np.pi, 5 * np.pi, 500)
+# f_x = x * (np.sin(x) ** 2)
+# g_x = -x * (np.sin(x) ** 2)
+
+# plt.title('Trigonometric Functions')
+# plt.xlabel('x-axis')
+# plt.ylabel('y-axis')
+# plt.plot(x, f_x, label='$f(x) = x\sin^2(x)$')
+# plt.plot(x, g_x, label='$g(x) = -x\sin^2(x)$')
+# plt.legend()
+# plt.show()
+######################## QUESTION 2 #############################
+# handling running in vscode or through terminal
+# try:
+#     file = open('./media/tcc.txt', 'r')
+# except:
+#     file = open('../../media/tcc.txt', 'r')
+# text = file.read().lower()
+
+# frequency = {}
+# for char in text:
+#     if char.isalpha() and char not in frequency:
+#         frequency[char] = 1
+#     elif char.isalpha():
+#         frequency[char] += 1
+
+# frequency = sorted(frequency.items(), key=lambda item: item[1])
+
+# letters, frequency = zip(*frequency)
+# print(letters)
+# print(frequency)
+
+# plt.bar(letters, frequency)
+# plt.title('Character Frequency in "The Cosmic Computer" by Piper')
+# plt.xlabel('Alphabet')
+# plt.ylabel('Frequency')
+# plt.show()
+######################## QUESTION 3 #############################
+# board = np.arange(8*8).reshape(8, 8) % 2
+# # went with board[::2] instead of board[1::2] so letters are correct
+# board[1::2] = (board[::2] + 1) % 2 
+
+# # literally just cuz i wanna be special
+# colors = ["#26033d", "#d38f2a"] # [purple, brown]
+# colorMap = lcm(colors)
+
+# plt.imshow(board, cmap=colorMap)
+# plt.title('Chess Board Pattern')
+# plt.xticks(np.arange(8), list('ABCDEFGH'))
+# plt.yticks(np.arange(8), np.arange(8, 0, -1))
+# plt.show()
+######################## QUESTION 3 #############################
+# a = np.matrix('1 0 1;2 1 1;0 1 1;1 1 2')
+# b = np.matrix('1 2 1;2 3 1;4 2 2')
+
+# ab = a * b
+
+# print(ab)
+######################## QUESTION 3 #############################
+n = np.linspace(1, 100)
+
+f_n = np.cumsum(())
